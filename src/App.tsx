7ce4e0b335577c9018e951d3c6974872688a1bac
4bh16am <120167005+4bh1zd@users.noreply.github.com>
2024-03-24 23:39:06 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import type { Component } from "solid-js";
+import Nav from "./assets/components/Nav";
+
+
+const App: Component = () => {
+  return (
+    <div>
+      <Nav/>
+      <h1>App Content</h1>
+    </div>
+  );
+};
+
+export default App;
